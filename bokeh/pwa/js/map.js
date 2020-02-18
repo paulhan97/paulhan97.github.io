127709428de0494ddad8f51dadf5a8650079110c
paulhan97 <paul.seungoh.han@gmail.com>
2020-02-18 12:59:49 -0500

    phone test

---
+++ bokeh/pwa/js/map.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  var map;
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 15
+    });
+  }
+
+});
